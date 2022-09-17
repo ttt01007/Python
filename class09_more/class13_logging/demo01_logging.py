8361f7dfe68c125aef6f61d0e7a94eac9743279e
ttt01007 <398847378@qq.com>
2022-09-17 16:30:36 +0800

    基础

--- class09_more/class13_logging/demo01_logging.py
+++ class09_more/class13_logging/demo01_logging.py
@@ -1,1 +1,16 @@
+# loggers:日志器
+# handler:处理器
+# formatter:格式器
+import logging
 
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(filename='log.log', filemode='w+', format=LOG_FORMAT,
+                    level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S',
+                    )
+logging.debug("This is a debug log.")
+logging.info("This is a info log.")
+logging.warning("This is a warning log.")
+logging.error("This is a error log.")
+logging.critical("This is a critical log.")
+
+logging.warning("Some one delete the log file.", exc_info=True, stack_info=True, extra={'user': 'Tom', 'ip':'47.98.53.222'})
---
+++ class09_more/class13_logging/demo02_logger.py
@@ -0,0 +1,23 @@
+import datetime
+import logging
+import logging.handlers
+
+logger1 = logging.getLogger('logger1')
+logger1.setLevel(logging.DEBUG)
+
+rf_handler = logging.handlers.TimedRotatingFileHandler('all.log', when='midnight', interval=1, backupCount=7,
+                                                       atTime=datetime.time(0, 0, 0, 0))
+rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+f_handler = logging.FileHandler('error.log')
+f_handler.setLevel(logging.ERROR)
+f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+
+logger1.addHandler(rf_handler)
+logger1.addHandler(f_handler)
+
+logger1.debug('debug message')
+logger1.info('info message')
+logger1.warning('warning message')
+logger1.error('error message')
+logger1.critical('critical message')
