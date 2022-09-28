8524634f66ed29dd9aa0ee36b78a51b6b57cfcbc
ttt01007 <398847378@qq.com>
2022-09-28 17:14:16 +0800

    基础

--- python_base/class13_logging/demo01_logging.py
+++ python_base/class13_logging/demo01_logging.py
@@ -2,6 +2,11 @@
 # handler:处理器
 # formatter:格式器
 import logging
+import logging.config
+
+# logging.config.fileConfig('log.ini')
+# log = logging.getLogger()
+# print(type(log), log)
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(filename='log.log', filemode='w+', format=LOG_FORMAT,
@@ -13,4 +18,5 @@ logging.warning("This is a warning log.")
 logging.error("This is a error log.")
 logging.critical("This is a critical log.")
 
-logging.warning("Some one delete the log file.", exc_info=True, stack_info=True, extra={'user': 'Tom', 'ip':'47.98.53.222'})
+logging.warning("Some one delete the log file.", exc_info=True, stack_info=True,
+                extra={'user': 'Tom', 'ip': '47.98.53.222'})
--- python_base/class13_logging/demo02_logger.py
+++ python_base/class13_logging/demo02_logger.py
@@ -5,6 +5,7 @@ import logging.handlers
 logger1 = logging.getLogger('logger1')
 logger1.setLevel(logging.DEBUG)
 
+# 根据时间，每天0点进行切割日志
 rf_handler = logging.handlers.TimedRotatingFileHandler('all.log', when='midnight', interval=1, backupCount=7,
                                                        atTime=datetime.time(0, 0, 0, 0))
 rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
@@ -13,8 +14,14 @@ f_handler = logging.FileHandler('error.log')
 f_handler.setLevel(logging.ERROR)
 f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
 
+s_handler = logging.StreamHandler()
+s_handler.setLevel(logging.ERROR)
+s_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+
+
 logger1.addHandler(rf_handler)
 logger1.addHandler(f_handler)
+logger1.addHandler(s_handler)
 
 logger1.debug('debug message')
 logger1.info('info message')
