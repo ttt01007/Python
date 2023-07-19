75e001ae510202cfc78c281deca4576b4e8541ac
ttt01007 <398847378@qq.com>
2023-07-19 11:11:14 +0800

    提交测试

--- python_01_base/class01_python_explain/class01/demo01_import.py
+++ python_01_base/class01_python_explain/class01/demo01_import.py
@@ -1,10 +1,12 @@
+import Python.python_01_base.class01_python_explain.class02.demo02
 
-
-python_base.class01_python_explain.class02.demo02.test02()
+Python.python_01_base.class01_python_explain.class02.demo02.test02()
 
 from Python.python_01_base.class01_python_explain.class02.demo02 import test02, test03
 
-test02()
-test03()
+
+
+
 
 test02()
+test03()
