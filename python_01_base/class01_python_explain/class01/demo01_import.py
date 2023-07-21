701aa43c39116b511bdbb5186a11164285acbb56
ttt01007 <398847378@qq.com>
2023-07-21 11:13:29 +0800

    提交测试2

--- python_01_base/class01_python_explain/class01/demo01_import.py
+++ python_01_base/class01_python_explain/class01/demo01_import.py
@@ -5,5 +5,16 @@ Python.python_01_base.class01_python_explain.class02.demo02.test02()
 from Python.python_01_base.class01_python_explain.class02.demo02 import test02, test03
 
 
+
+
+
+
+
+
+
+
+
+
+
 test02()
 test03()
